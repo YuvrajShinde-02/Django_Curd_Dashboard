b4453dbd23f26e156ccf6519a92deed32c2f2cc4
Yuvraj Shinde <yuvraj.shinde@webaccessglobal.com>
2025-09-10 19:22:45 +0530

    Update settings.py

--- simplecrm/settings.py
+++ simplecrm/settings.py
@@ -79,17 +79,21 @@ ASGI_APPLICATION = 'simplecrm.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'simplecrm',
+#         'USER': 'simpleuser',
+#         'PASSWORD': 'simplepass',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'simplecrm',
-        'USER': 'simpleuser',
-        'PASSWORD': 'simplepass',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
